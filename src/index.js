d0bd307f91b2f0b5c59cb29b1ca6159937fe1c60
Sergio López Rey <sergiolopezdanielrey@gmail.com>
2024-04-06 16:32:13 -0300

    startup page, very simple C:

--- src/index.js
+++ src/index.js
@@ -5,10 +5,23 @@ import { enviroment } from './utils/enviroment.js'
 import SpyController from './controllers/spyController.js'
 import TweetController from './controllers/tweetController.js'
 import { cronScheduler } from './services/cronServices.js'
+import path from 'path'
+import { fileURLToPath } from 'url'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+import { getLastSpyValue } from './repository/SpyValues.js'
+import nunjucks from 'nunjucks'
+import { moneyParse } from './utils/parser.js'
 
 const app = express()
 const port = enviroment.port || 8080
 
+nunjucks.configure(`${__dirname}/views`, {
+  autoescape: true,
+  express: app
+})
+
+app.set('view engine', 'njk')
 app.listen(port, () => {
   mongoose.connect(enviroment.mongoConnectionString).then(() => {
     console.log('Connected to mongo DB')
@@ -20,3 +33,9 @@ app.listen(port, () => {
 app.use('/api/v1/spy', SpyController)
 
 app.use('/api/v1/tweet', TweetController)
+
+app.get('/', async (req, res) => {
+  const { value } = await getLastSpyValue()
+  const parsed = moneyParse(value)
+  res.render('home', { value: parsed })
+})
